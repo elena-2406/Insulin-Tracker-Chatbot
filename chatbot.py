e7b5fe0219f7cf94852d79245e622c94bd768bca
Elena <annmats2006@gmail.com>
2025-09-02 16:02:11 +0530

    Inserting telegram token for the bot

--- chatbot.py
+++ chatbot.py
@@ -2,3 +2,6 @@ import telebot
 import time
 import firebase_admin
 from firebase_admin import credentials, db
+#Inserting telegram token
+TOKEN = "8368529325:AAGNA8gpzkXwSdJyajxWKKC04p_lq-66rxk"
+bot = telebot.TeleBot(TOKEN)
